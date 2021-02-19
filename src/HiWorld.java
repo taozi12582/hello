ea9f82a656a04481dc53a7a6bf51bfebc833c21c
S9037218 <Orangejuice135!>
2021-02-19 16:00:32 +0800

    testsfv

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,6 +1,6 @@
 public class HiWorld {
     public static void main(String args[]){
         System.out.println("Hi World");
-        System.out.println("ascasaxcwaszcas线程sdsvdsdassws");
+        System.out.println("ascasaxcvwaszcas线程sdsvdsdassws");
     }
 }
