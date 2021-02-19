14a0d033838ac68118a13359819fe217893c315e
S9037218 <Orangejuice135!>
2021-02-19 10:01:14 +0800

    asas

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,6 +1,6 @@
 public class HiWorld {
     public static void main(String args[]){
         System.out.println("Hi World");
-        System.out.println("ascascas");
+        System.out.println("ascascass");
     }
 }
