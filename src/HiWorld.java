8f49d36f36942d345956cf4df94ea226c1de128c
S9037218 <Orangejuice135!>
2021-02-20 16:29:56 +0800

    zxc

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,7 +1,7 @@
 public class HiWorld {
     public static void main(String args[]){
         System.out.println("Hi World");
-        System.out.println("new hbhbhbhb");
+        System.out.println("new hchchc");
 
     }
 }
