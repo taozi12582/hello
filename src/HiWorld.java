a87bda4b2b8739f07f725a41719a978979f49d25
S9037218 <Orangejuice135!>
2021-02-20 10:11:47 +0800

    hihi

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,7 +1,7 @@
 public class HiWorld {
     public static void main(String args[]){
         System.out.println("Hi World");
-        System.out.println("new ha");
+        System.out.println("new haha");
 
     }
 }
