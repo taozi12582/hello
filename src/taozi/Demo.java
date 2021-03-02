a44267621941d6b989f73c0364e54335eec3a79e
S9037218 <Orangejuice135!>
2021-03-02 16:59:23 +0800

    zxc

---
+++ src/taozi/Demo.java
@@ -0,0 +1,35 @@
+package taozi;
+
+import java.util.List;
+
+public class Demo {
+
+    /**
+     * test1111122
+     */
+    private String test1;
+
+
+
+    private List<Demo1> demo1List;
+
+    public List<Demo1> getDemo1List() {
+        return demo1List;
+    }
+
+    public void setDemo1List(List<Demo1> demo1List) {
+        this.demo1List = demo1List;
+    }
+
+    public String getTest() {
+        return test1;
+    }
+
+    public void setTest(String test) {
+        this.test1 = test;
+    }
+
+
+
+}
+
