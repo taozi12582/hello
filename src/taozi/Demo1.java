842fd6480fdf3f734ec091fcc1f8bb7e7ee49ec5
S9037218 <Orangejuice135!>
2021-03-04 15:21:36 +0800

    zxc

---
+++ src/taozi/Demo1.java
@@ -0,0 +1,14 @@
+package taozi;
+
+public class Demo1 {
+
+    private String demo1;
+
+    public String getDemo1() {
+        return demo1;
+    }
+
+    public void setDemo1(String demo1) {
+        this.demo1 = demo1;
+    }
+}
