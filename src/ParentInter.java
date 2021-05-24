d1c93ae130afcbab458b8d05720340e7fce807f7
S9037218 <Orangejuice135!>
2021-05-24 18:10:23 +0800

    zxc

--- src/ParentInter.java
+++ src/ParentInter.java
@@ -8,5 +8,5 @@ public interface ParentInter{
     MidInter m = null;
     int a = 2;
     void finalSuccess();
-    public boolean asas(String a11e3s3);
+    public boolean asas(String a11e3s3s);
 }
